ece558b5bc1bbc33dd52e895b1d7e4ac540bc5ec
Travis Peters <traviswp@gmail.com>
2017-12-24 13:31:01 -0500

    updating the interactive quiz include style

--- toys/interactive-quiz/script.js
+++ toys/interactive-quiz/script.js
@@ -1,24 +1,3 @@
-// List of questions (statement + answer)
-const questions = [
-  {
-    statement: "2+2?",
-    answer: "2+2 = 4"
-  },
-  {
-    statement: "In which year did Christopher Columbus discover America?",
-    answer: "1492"
-  },
-  {
-    statement: "What is the answer to life, the universe, and everything?",
-    answer: "42"
-  },
-  {
-    statement:
-      "What occurs twice in a lifetime, but once in every year, twice in a week but never in a day?",
-    answer: "The E letter"
-  }
-];
-
 // Web page loading event
 window.addEventListener("load", e => {
     const content = document.getElementById("content");
